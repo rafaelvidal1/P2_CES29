7242bcd504f0e4fe37aa8b1fe918ef410c210c06
Rafael Vidal <Rafael Vidal>
2017-07-01 15:41:57 -0300

    Mudancas feitas na classe da Q2

--- P2_Ex2/src/exe2/VulnerableClass.java
+++ P2_Ex2/src/exe2/VulnerableClass.java
@@ -6,17 +6,31 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class VulnerableClass {
 	public void vulnerableMethod(String FILENAME){
+		
+		Pattern pattern = Pattern.compile("[^A-Za-z0-9._]");
+		Matcher matcher = pattern.matcher(FILENAME);
+		
+		if(matcher.find()){
+			System.out.print("O nome do arquivo contem caracteres problematicos, por favor, mude o nome.");
+			return;
+		}
+		
 		while (true) {
 		    Scanner console = new Scanner(System.in);
 		    System.out.print("Digite a operacao desejada para realizar no arquivo <R para ler um arquivo, "
 		    		+ "W para escrever em um arquivo>? ");
 			
 		    String opr= console.nextLine();
+		    console.close();
 			
-		    if (opr.equals("R")){
+		    switch (opr){
+		    
+		    case "R":
 				BufferedReader br = null;
 				FileReader fr = null;
 				
@@ -24,12 +38,10 @@ public class VulnerableClass {
 
 					fr = new FileReader(FILENAME);
 					br = new BufferedReader(fr);
-
-					String sCurrentLine;
-
 					br = new BufferedReader(new FileReader(FILENAME));
 
-					while ((sCurrentLine = br.readLine()) != null) {
+					while (br.readLine() != null) {
+						String sCurrentLine = br.readLine();
 						System.out.println(sCurrentLine);
 					}
 
@@ -37,23 +49,26 @@ public class VulnerableClass {
 
 					e.printStackTrace();
 
-				} 
-			}
-			
-			else {
-				  BufferedWriter buffWrite;
-				  
-				  try {
+				}
+				break;
+		    case "W": 
+		    	BufferedWriter buffWrite;
+					  
+				try {
 					buffWrite = new BufferedWriter(new FileWriter(FILENAME));
 					String linha = "";
 					System.out.println("Escreva algo: ");
-				    linha = console.nextLine();
-				    buffWrite.append(linha + "\n");
-				     
-				} catch (IOException e) {
+					linha = console.nextLine();
+					console.close();
+					buffWrite.append(linha + "\n");
+					     
+				}catch (IOException e) {
 					e.printStackTrace();
 				} 
-			}
+				break;
+			default:
+				System.out.println("Digite uma opcao valida!\n");
+		    }
 		}
 	}
 }
